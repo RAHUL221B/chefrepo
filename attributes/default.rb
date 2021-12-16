50b32e8fd0cb2b0fe5512f068fad773438cc48e3
rahul <rrpatil444@gmail.com>
2021-12-16 05:32:03 +0530

    added attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[apache_phpinfo][apache_package]='apache2'\
+default[apache_phpinfo][apache_package]='apache2'
 default[apache_phpinfo][all_packages]=%w(apache2 php libapache2-mod-php php-mysql)
 default[apache_phpinfo][info_page_path]='/var/www/html/info.php'
 
