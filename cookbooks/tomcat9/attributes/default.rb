c014c9968f76c914cc7186134ae04c1fd8cea840
rahul <rrpatil444@gmail.com>
2021-12-18 06:00:17 +0530

    added tomcat 9

---
+++ cookbooks/tomcat9/attributes/default.rb
@@ -0,0 +1,4 @@
+
+if platform?('ubuntu')
+    defult['tomcat9']['java_package'] = 'openjdk-11-jdk'
+end    
---
+++ cookbooks/tomcat9/recipes/java.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: tomcat9
+# Recipe:: java
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+apt_update 'update ubuntu packages' do
+ ignore_failure true
+ action :update
+ only_if {platform?('ubuntu')}
+end
+
+
+package node ['tomcat9']['java_package'] do
+    action :install
+end
