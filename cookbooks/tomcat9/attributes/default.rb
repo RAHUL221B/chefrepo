946390061a208b12cfa09948991940f69fe2f1da
rahul <rrpatil444@gmail.com>
2021-12-18 06:30:11 +0530

    added receipie to configure tomcat

--- cookbooks/tomcat9/attributes/default.rb
+++ cookbooks/tomcat9/attributes/default.rb
@@ -2,3 +2,9 @@
 if platform?('ubuntu')
     defult['tomcat9']['java_package'] = 'openjdk-11-jdk'
 end    
+
+
+default['tomcat9']['username'] = 'tomcat'
+default['tomcat9']['groupname'] = 'tomcat'
+default['tomcat9']['userhome'] = '/opt/tomcat'
+default['tomcat9']['usershell'] = '/bin/false'
