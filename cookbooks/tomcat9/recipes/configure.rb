0292a26060531f8e35a71e864af32b1c3c4d2332
rahul <rrpatil444@gmail.com>
2021-12-18 20:33:16 +0530

    Deploying tomcat 11 nov

--- cookbooks/tomcat9/recipes/configure.rb
+++ cookbooks/tomcat9/recipes/configure.rb
@@ -9,9 +9,25 @@
 
 
 
+user_home = node['tomcat9']['userhome']
+
 user node['tomcat9']['username'] do
-    comment 'comment'
-    home node['tomcat9']['userhome']
-    shell node['tomcat9']['usershell']
-    action :create
+  comment 'comment'
+  home user_home
+  shell node['tomcat9']['usershell']
+  action :create
+end
+
+download_url = node['tomcat9']['download_url']
+local_download_path = node['tomcat9']['local_download_path']
+
+remote_file local_download_path do
+  source download_url
+  action :create
+end
+
+archive_file 'tomcat' do
+  path local_download_path
+  destination user_home
+  action :extract
 end
